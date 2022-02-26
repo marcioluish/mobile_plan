fa48f933d72ccc962be0f1f44a1de6ab0f670c4a
Marcio Luis Hamasaki da silva <marcio_luis@live.com>
2022-02-26 14:00:39 -0300

    [BACKEND] Add initial structure NodeJS+Express and MongoDB.

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const http = require('http');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+const server = http.createServer(app);
+
+const accessLogStream = fs.createWriteStream(
+    path.join(__dirname, 'logs', 'access.log'),
+    { flags: 'a' }
+);
+
+app.use(morgan('combined', { stream: accessLogStream }));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+mongoose.connect(
+    `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mplan_db:27017?authSource=admin`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err) => {
+        if (err) {
+            console.error('failed to connect to DB');
+            console.error(err);
+        } else {
+            console.log('connected to DB');
+            server.listen(8000);
+        }
+    }
+);
